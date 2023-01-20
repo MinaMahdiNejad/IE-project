301b36b011de11d9c0f45ebc34a6f1bf1293c786
MinaMahdiNejad <minamn.111380@gmail.com>
2023-01-20 23:07:42 +0330

    SD

--- src/main/java/com/example/demo/controller/SourceDestination.java
+++ src/main/java/com/example/demo/controller/SourceDestination.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
